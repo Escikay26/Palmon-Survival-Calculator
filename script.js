09ea81a01291fab7dee6e86cae2900f50d0d0ed2
Escikay26 <spam.kamburg@gmx.de>
2026-09-04 14:33:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+console.log("Achievement Planner loaded!");
 
+const elementSelect = document.getElementById("element");
+
+elementSelect.addEventListener("change", function () {
+  console.log("Selected element:", elementSelect.value);
+});
